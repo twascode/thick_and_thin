cab075920430e6f2c39fe8fb7c3925b4c873cae9
nivla <1nivla1alvin@gmail.com>
2023-06-09 21:14:38 +0700

    update animation

--- src/js/index.js
+++ src/js/index.js
@@ -223,45 +223,112 @@ ScrollTrigger.create({
     className: "slide-in-right",
   },
 });
-videojs.getPlayer("myPlayerID").ready(function () {
-  // +++ Initialize variables and check for mobile device +++
-  var player = this,
-    playerContainer = document.getElementById("videoContainer"),
-    isMobile =
-      /Android|webOS|iPhone|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(
-        navigator.userAgent
-      )
-        ? true
-        : false;
-
-  if (isMobile) {
-    // +++ Define a modal close button +++
-    var CloseModal = videojs.getComponent("button");
-    var CloseModal_ = videojs.extend(CloseModal, {
-      constructor: function () {
-        CloseModal.apply(this, arguments);
-        this.addClass("vjs-close-modal");
-        this.controlText("Close video");
-      },
-      handleClick: function () {
-        playerContainer.style.maxWidth = "286px";
-      },
-    });
-
-    videojs.registerComponent("CloseModal", CloseModal_);
-    player.addChild("CloseModal", {});
-
-    // +++ When playback begins, enter full width mode +++
-    player.on("play", function () {
-      playerContainer.style.width = "100%";
-      playerContainer.style.maxWidth = "";
-    });
-  }
-
-  // +++ When playback ends, exit full width mode +++
-  player.on("ended", function () {
-    playerContainer.style.maxWidth = "286px";
-    player.currentTime(0);
-  });
+ScrollTrigger.create({
+  trigger: ".pict-5",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-5ar",
+    className: "slide-in-right",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-5",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-5cr",
+    className: "rotate-in-2-tr-cw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-5",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-5bl",
+    className: "slide-in-left",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-5",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-5cl",
+    className: "rotate-in-2-tl-ccw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-5",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-5al",
+    className: "rotate-in-2-tl-ccw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-5",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-5br",
+    className: "rotate-in-2-tr-cw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-6",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-6ar",
+    className: "rotate-in-2-tr-cw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-6",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-6cr",
+    className: "rotate-in-2-tr-cw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-6",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-6bl",
+    className: "rotate-in-2-tl-ccw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-6",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-6cl",
+    className: "rotate-in-2-tl-ccw",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-6",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-6al",
+    className: "slide-in-left",
+  },
+});
+ScrollTrigger.create({
+  trigger: ".pict-6",
+  start: "-=600",
+  end: "+=1050",
+  toggleClass: {
+    targets: "#pict-6br",
+    className: "slide-in-right",
+  },
 });
 rellax.refresh();
